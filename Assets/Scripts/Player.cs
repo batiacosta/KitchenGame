5c26887e5cfad8b1eaf8afe3c8f04d0e26812cc0
David Leonardo Acosta Laverde <david.acosta.ing@gmail.com>
2023-02-06 18:12:09 -0500

    Refactorization using new Input System
    Adding arrows and gamepad support

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,28 +7,13 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float _movementSpeed = 0;
+    [SerializeField] private GameInput _gameInput;
 
     private bool _isWalking = false;
     private void Update()
     {
-        Vector2 inputVector = new Vector2(0, 0);
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = + 1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = - 1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = - 1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = + 1;
-        }
 
+        Vector2 inputVector = _gameInput.GetMovementVectorNormalized();
         inputVector = inputVector.normalized;
 
         Vector3 movementDirection = new Vector3(inputVector.x, 0, inputVector.y);
