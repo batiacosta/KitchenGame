d07e8302b6c8f8bc4d1db82aa49eb3133f9ef7a8
David Leonardo Acosta Laverde <david.acosta.ing@gmail.com>
2023-02-15 11:55:56 -0500

    Adding valid food to a plate being carried by player

--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -36,6 +36,19 @@ public class KitchenObject : MonoBehaviour
         return  _kitchenObjectParent;
     }
 
+    public bool TryGetPlate(out PlateKitchenObject plateKitchenObject)
+    {
+        if (this is PlateKitchenObject)
+        {
+            plateKitchenObject = this as PlateKitchenObject;
+            return true;
+        }
+        else
+        {
+            plateKitchenObject = null;
+            return false;
+        }
+    }
     public void DestroySelf()
     {
         _kitchenObjectParent.ClearKitchenObject();
---
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PlateKitchenObject : KitchenObject
+{
+    [SerializeField] private List<KitchenObjectSO> _validKitchenObjectSOList;
+    private List<KitchenObjectSO> _kitchenObjectList;
+
+    private void Awake()
+    {
+        _kitchenObjectList = new List<KitchenObjectSO>();
+    }
+
+    public bool TryAddIngredient(KitchenObjectSO kitchenObjectSO)
+    {
+        if (!_validKitchenObjectSOList.Contains(kitchenObjectSO))
+        {
+            return false;   //  Not a valid ingredient
+        }
+        if (_kitchenObjectList.Contains(kitchenObjectSO))
+        {
+            //  Already has this type
+            return false;
+        }
+        else
+        {
+            _kitchenObjectList.Add(kitchenObjectSO);
+            return true;
+        }
+        
+    }
+}
