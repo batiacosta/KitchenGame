14dde992b0d0a5f41b4d2831ebe3f110b12632fa
David Leonardo Acosta Laverde <david.acosta.ing@gmail.com>
2023-05-08 18:58:45 -0500

    GameManager state machine

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private enum State
+    {
+        WaitingToStart, //  Waiting until all players are connected
+        CountdownToStart,
+        GamePlaying,
+        GameOver,
+    }
+
+    private State _state;
+    private float _waitingToStartTimer = 1f;
+    private float _countdownToStartTimer = 3f;
+    private float _gamePlayingTimer = 10f;
+    private void Awake()
+    {
+        _state = State.WaitingToStart;
+    }
+
+    private void Update()
+    {
+        switch (_state)
+        {
+         case State.WaitingToStart:
+             _waitingToStartTimer -= Time.deltaTime;
+             if (_waitingToStartTimer < 0f)
+             {
+                 _state = State.CountdownToStart;
+             }
+             break;
+         case State.CountdownToStart:
+             _countdownToStartTimer -= Time.deltaTime;
+             if (_countdownToStartTimer < 0f)
+             {
+                 _state = State.GamePlaying;
+             }
+             break;
+         case State.GamePlaying:
+             _gamePlayingTimer -= Time.deltaTime;
+             if (_gamePlayingTimer < 0f)
+             {
+                 _state = State.GameOver;
+             }
+             break;
+         case State.GameOver:
+             break;
+        }
+    }
+}
