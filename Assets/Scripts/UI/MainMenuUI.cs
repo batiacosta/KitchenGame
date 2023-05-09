54a163580a5b9de6587db9dbada77a47a67b6ce3
David Leonardo Acosta Laverde <david.acosta.ing@gmail.com>
2023-05-09 10:49:20 -0500

    MainMenuUI Layout and basic function

---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button quitButton;
+
+    private void Awake()
+    {
+        playButton.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(1);
+        });
+        quitButton.onClick.AddListener(() =>
+        {
+            Application.Quit();
+#if UNITY_EDITOR
+            
+#endif
+        });
+    }
+    
+}
